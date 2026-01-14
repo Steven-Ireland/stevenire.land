a141d2ff5f865990c5bc6d6d2e2041cdc1dcd1bf
Steven Ireland <steven.ireland@protonmail.com>
2026-01-14 17:55:48 -0500

    style changes

--- src/components/BackgroundEffect.js
+++ src/components/BackgroundEffect.js
@@ -22,7 +22,7 @@ const BackgroundEffect = ({
       width: '100%',
       height: '100%',
       pointerEvents: 'none',
-      zIndex: 9999,
+      zIndex: -1,
       overflow: 'hidden',
     }}>
       
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Banner from './Banner';
 import Footer from './Footer';
 import BackgroundEffect from './BackgroundEffect';
@@ -6,6 +7,9 @@ import BackgroundEffect from './BackgroundEffect';
 const Layout = ({ children }) => {
   return (
     <div className="Page">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <BackgroundEffect 
         scanlines={true} 
         vignette={true} 
